8d8c4c93056becce3e9080191298b5dbeffe7cc3
bfabien99 <fabienbrou99@gmail.com>
2022-11-23 12:49:02 +0000

    📝 ajout des données dans la base de donnée avec les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,23 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use Faker\Factory;
+use App\Entity\Ingredient;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+        for ($i = 1; $i <= 50; $i++) {
+            $ingredient = new Ingredient();
+            $ingredient->setName($faker->word())
+                ->setPrice(mt_rand(0,199));
+
+            $manager->persist($ingredient);
+        }
 
         $manager->flush();
     }
